5e4303043b17fad78bdef8aac4c932660552aa4c
root <root@www.ben-rush.net>
2015-09-16 13:27:55 -0400

    Will now save id and link. Also code cleaned up a bit.

--- logintest.py
+++ logintest.py
@@ -6,8 +6,29 @@ import time
 conn = sqlite3.connect('coupons')
 cursor = conn.cursor()
 
+cursor.execute('delete from coupon')
+conn.commit()
+
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(10) # wait if not readily available
+
+def HandlePage(toInsert, browser, link):
+
+	# get the root coupon divs
+        couponItemsList = browser.find_elements_by_xpath(\
+                "//div[@class='coupons-list']/div")
+
+	# for each div get the information, append it to the array. 
+        for couponItem in couponItemsList:
+                idOfCouponItem = couponItem.get_attribute('id').replace('ItemHdr_','')
+                couponFields = list(couponItem.find_elements_by_xpath(".//div[@class='inner-coupon-div']/*[2]/div"))
+
+                title = couponFields[0].text
+                save = couponFields[1].text
+                description = couponFields[2].text
+                expires = couponFields[3].text
+	
+		toInsert.append([title, save, description, expires, link, int(idOfCouponItem)])
 
 if browser is not None: 
 	browser.get('https://secure3.mywebgrocer.com/UMA/RegisterSignIn.aspx?uc=AEEAF106')
@@ -20,41 +41,32 @@ if browser is not None:
 	password.send_keys('###')
 
 	loginForm.submit()
-
-	browser.get('http://super-saver.mywebgrocer.com/Coupons.aspx')
-
-	allCouponLinkElements = browser.find_elements_by_xpath(\
-		"//div[@class='coupons-pages ui-corner-all']/*[1]/a[@class='coupons-nav']")
-
+	
 	allCouponLinks = []
-
-	for couponLink in allCouponLinkElements:
-		allCouponLinks.append(couponLink.get_attribute('href'))
-
 	toInsert = []
 
+	mainPageLink = 'http://super-saver.mywebgrocer.com/Coupons.aspx'
+
+	# get the main coupon page. load the links to the other coupon pages. 
+	browser.get(mainPageLink)
+	allCouponLinkElements = browser.find_elements_by_xpath(\
+		"//div[@class='coupons-pages ui-corner-all']/*[1]/a[@class='coupons-nav']")
+	for couponLink in allCouponLinkElements:
+		allCouponLinks.append(couponLink.get_attribute('href'))
+	
+	# get the coupon records on this page
+	HandlePage(toInsert, browser, mainPageLink)
+
+	# for each subsequent coupon page, load it and pull down the coupon records
 	for couponLink in allCouponLinks:
 		print('Navigating to ' + couponLink)
-
+		# load
 		browser.get(couponLink)
+		# handle
+		HandlePage(toInsert, browser, mainPageLink)
 	
-		allCouponDivs = browser.find_elements_by_xpath(\
-			"//div[@class='coupon-description-container float-right']")	
-
-		for couponDiv in allCouponDivs:
-			titleDiv = couponDiv.find_element_by_xpath(".//*[1]/div")
-			saveDiv = couponDiv.find_element_by_xpath(".//*[2]/div")			
-			descriptionDiv = couponDiv.find_element_by_xpath(".//*[3]/div")
-			expiresDiv = couponDiv.find_element_by_xpath(".//*[4]/span")			
-
-			title = titleDiv.text
-			save = saveDiv.text
-			description = descriptionDiv.text
-			expires = expiresDiv.text.replace("Coupon expires: ","")
-
-			toInsert.append([title, save, description, expires])
-	
-	cursor.executemany('insert into coupon values (?,?,?,?)', toInsert)
+	# write to the database		
+	cursor.executemany('insert into coupon values (?,?,?,?,?,?)', toInsert)
 else:
 	print('failed')
 
